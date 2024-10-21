5387343883acbd51ac1743eec80ba8b15276d9b3
Arun2003-pixel <akuriarunkumar@gmail.com>
2024-10-21 11:35:56 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf(" ENTER THE NUMBERS FOR a , b , c : ");
+scanf("%d""%d""%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf("%d is largest",a);
+}
+}
+else if (b>c)
+{
+printf("%d is largest",b);
+}
+else
+{
+printf("%c is largest");
+}
+}
